b90d949282cd618e8e6a268c377676c0349cd603
crestrepo <cristina.restrepo@trivago.com>
2019-01-14 05:03:11 +0100

    Build production

---
+++ build/precache-manifest.6e10a253602cb54fefc2b7c5df5a1716.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "79ca1a1d2da3a19b98cf",
+    "url": "/static/js/main.79ca1a1d.chunk.js"
+  },
+  {
+    "revision": "76df0786cbe35ca7d204",
+    "url": "/static/js/1.76df0786.chunk.js"
+  },
+  {
+    "revision": "79ca1a1d2da3a19b98cf",
+    "url": "/static/css/main.c9fe78d6.chunk.css"
+  },
+  {
+    "revision": "e703cdcefac8846cc9165ff2919510c2",
+    "url": "/index.html"
+  }
+];
